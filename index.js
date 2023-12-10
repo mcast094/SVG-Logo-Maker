518e55c0a61298fe5c3ac64e8bb2567740426434
Martin Castaneda <martincastaneda75@yahoo.com>
2023-12-10 15:19:18 -0800

    take input from console

---
+++ index.js
@@ -0,0 +1,8 @@
+var charInput = readLine();
+
+console.log(`Please enter three characters for your logo: ${charInput}`);
+
+console.log("Enter a color for your logo text: ");
+console.log("Please enter a shape for your logo (circle, triangle, or square): ");
+console.log("Please enter a color for the shape entered: ");
+
